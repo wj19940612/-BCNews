d66e80d654f52ecb74b9d3d911140e7c68926a3f
John Zhang <zxukai@gmail.com>
2018-01-18 11:03:54 +0800

    First commit

---
+++ app/src/main/java/com/sbai/bcnews/BaseActivity.java
@@ -0,0 +1,14 @@
+package com.sbai.bcnews;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Modified by john on 18/01/2018
+ * <p>
+ * Description:
+ * <p>
+ * APIs:
+ */
+public class BaseActivity extends AppCompatActivity {
+
+}
