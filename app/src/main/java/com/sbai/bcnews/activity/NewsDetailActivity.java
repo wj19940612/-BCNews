febef1a1bfafdbd808f77ae46bc3788db1a883d8
zhangjianfeng <zhangjianfeng@hashxy.com>
2018-01-29 18:19:58 +0800

    资讯列表

--- app/src/main/java/com/sbai/bcnews/activity/NewsDetailActivity.java
+++ app/src/main/java/com/sbai/bcnews/activity/NewsDetailActivity.java
@@ -31,6 +31,7 @@ import com.sbai.bcnews.utils.DateUtil;
 import com.sbai.bcnews.utils.Launcher;
 import com.sbai.bcnews.utils.ToastUtil;
 import com.sbai.bcnews.utils.news.NewsCache;
+import com.sbai.bcnews.view.DrawWebView;
 import com.sbai.bcnews.view.NewsScrollView;
 import com.sbai.bcnews.view.TitleBar;
 
@@ -43,10 +44,10 @@ import butterknife.OnClick;
  */
 
 public class NewsDetailActivity extends BaseActivity {
-    public static final String text = "<p>财联社11月2日讯，随着威创股份的定增、红黄蓝的美股上市等，上市公司快速扩张，竞争激烈的幼教市场一直在跑马圈地。</p>\r\n\r\n<p>10月27日，威创股份发布公告称，已募集到9.18亿元资金，其中扣除发行费用后的募集资金净额9.06亿元，将全部投入到儿童艺体培训中心建设项目，占总投资额的比例为75.43%。</p>\r\n\r\n<p><strong>威创的幼教利润占其总利润的份额超过70%。</strong></p>\r\n\r\n<p>威创股份的前三季度报显示，招商证券、华创证券、中泰证券、天风证券等均给予推荐，或者买入的评级，这是在A股教育上市公司的评级中不多见的。</p>\r\n\r\n<p>不过，&ldquo;园所数量增长不及预期&rdquo;&ldquo; 外延收购不及预期&rdquo;等风险不容忽视，那么，威创积极并购幼教的后遗症是什么?对于跨界幼教的上市公司有哪些启示?</p>\r\n\r\n<p><strong>券商唱好背后，加盟模式是否健康</strong></p>\r\n\r\n<p>自2015年开始，威创股份从大屏幕数字显示系统专业供应商，通过收购红缨教育和金色摇篮切入幼教，仅收购幼儿园就已累计投入达18.68亿元。</p>\r\n\r\n<p><img alt=\"\" src=\"https://image.cailianpress.com/cailianpress/default/20171102/103839_59fa852f8aa15.jpg\" style=\"width: 640px; height: 411px;\" />&nbsp;</p>\r\n\r\n<p>通过快速并购，威创股份实现了净利润快速增长;线下并购当地有号召力的幼教品牌，投资线上企业幼师口袋，布局和培养幼教师资，为威创旗下其他品牌输出师资和管理人才。从收购类型来看，威创股份收购标的企业均偏资源型，旗下都有很多实体。</p>\r\n\r\n<p><img height=\"1\" src=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAOxAAADsQBlSsOGwAAABBJREFUCB0BBQD6/wAAAAAAAAUAAbqJEIoAAAAASUVORK5CYII=\" width=\"1\" />&nbsp;<img alt=\"\" src=\"https://image.cailianpress.com/cailianpress/default/20171102/103904_59fa8548cf406.jpg\" style=\"width: 640px; height: 144px;\" /></p>\r\n\r\n<p>威创股份的四起并购均设置了业绩承诺。2015年和2016年，金色摇篮和红缨教育的实际业绩完成情况均高于业绩承诺。红缨教育表现最好，2015年实际完成5648万元，2016年实际完成1.02亿元。</p>\r\n\r\n<p><strong>部分核心员工因利益分布不均，以及并购后分红减少而离开了红缨教育</strong></p>\r\n\r\n<p>一旦超额完成业绩达到一定水平，威创股份需要对核心管理层进行额外奖励。不过，一位红缨教育的中层员工向财联社透露，威创通过相关的资本力量促成了对红缨的收购，缺乏核心人才直指幼教的要害，尤其对于加盟园来说，核心的经营管理人才很重要。但是，加盟模式本身才是症灶。</p>\r\n\r\n<p><img height=\"1\" src=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAOxAAADsQBlSsOGwAAABBJREFUCB0BBQD6/wAAAAAAAAUAAbqJEIoAAAAASUVORK5CYII=\" width=\"1\" />&nbsp;<img alt=\"\" src=\"https://image.cailianpress.com/cailianpress/default/20171102/103923_59fa855bc35d7.jpg\" style=\"width: 544px; height: 44px;\" /></p>\r\n\r\n<p>加盟模式及其良性扩张是资本市场关注的。瑞金基金Z姓高管向财联社指出，加盟园所不需要获得幼儿园的所有权，只需要交加盟费，不涉及运营。虽然实现了轻资产的扩张并且扩张速度极快，但是很多加盟园没有进一步消化加盟品牌和管理方式。</p>\r\n\r\n<p>无独有偶，一位和君系合伙人认为，幼儿园是资本市场的优质资源，但是威创的园所大多采取加盟模式，如何提高加盟品牌的黏性以实现良好扩张，这是威创在并购后亟待考量的。</p>\r\n\r\n<p>同时，绝大多数的幼儿园资源原有的经营模式较弱，主要有两大风险，一个是并购后提高了幼儿园的管理成本;第二个在于幼儿园需要增强经营成本和收益，来反哺自身的管理成本。</p>\r\n\r\n<p>面对业绩承诺大考，并购后的幼儿园要&ldquo;自负盈亏&rdquo;，这对幼儿园的经营来说是个不小的考验。此外，上市公司也要考虑募资的成本、加盟园的投资回报率以及长期投资价值。</p>\r\n\r\n<p><strong>&ldquo;加盟模式基本进入尾声阶段。虽然短期内死不了，但是加盟模式是危险的。&rdquo;</strong></p>\r\n\r\n<p>阳光雨露的董事长徐玲表示，&ldquo;加盟模式有共性。首先，目前的连锁式园所规模大，单体园所数量少，加盟的市场空间变少;第二，品牌在加盟后的支持力度有待提高;另外，加盟还面临续费的问题。&rdquo;</p>\r\n\r\n<p>值得注意的是，曾被威创列入投资目标的幼教公司创始人告诉财联社，&ldquo;之所以没有接受威创主要在于，虽然威创表示发力成为文化教育平台，但是威创未来的主营业务并不清晰。另外，威创现在是双主业，原主业营收有所增长，一旦原主营业务增长快，会不会造成教育业务的边缘化。&rdquo;</p>\r\n\r\n<p><strong>剥离警示：跨界幼教不容易</strong></p>\r\n\r\n<p>纵观整个A股，财联社发现，涉及教育业务的公司对教育的态度不一。仅从剥离业务的角度来看，上市公司有三种主要行为：一类是剥离原有业务，转而发力教育;第二类是剥离教育业务，发力原有业务;第三类是一直从事教育业务，但会随市场需求对业务线进行调整。</p>\r\n\r\n<p><img alt=\"\" src=\"https://image.cailianpress.com/cailianpress/default/20171102/103951_59fa857758289.jpg\" style=\"width: 640px; height: 468px;\" /></p>\r\n\r\n<p>整体来看，威创的幼教行业盈利能力优于电子视像业务，幼教行业的收入增长成为带动公司营收增长的主要动力。</p>\r\n\r\n<p>除了威创股份，LED行业还有勤上股份、长方集团等跨界进入幼教。长方集团在2016年并购线下幼儿园、开设教育子公司，寻求&ldquo;LED+教育&rdquo;双主业发展。而勤上股份迈步最大，主业涉及幼教、K12课外辅导、国际教育以及民办学校等领域。2016年，勤上股份前董秘胡绍安曾向媒体表示，未来不排除剥离LED资产的可能性。</p>\r\n\r\n<p>&ldquo;透过与威创股份董事长何正宇沟通，可以感受到他对原主业的热爱，我个人认为他不会剥离原主业，这也是对并入之后的一个担心&rdquo;，曾参与并购谈判的幼教公司创始人如是说。</p>\r\n\r\n<p><strong>引入招商局和擅长</strong></p>\r\n\r\n<p>也有资深基金人士预测，同时，原主营虽然没有大幅度提升，但其业务能力仍表现良好，剥离原有主业的意义不大。</p>\r\n\r\n<p>威创股份董秘李亦争曾对财联社表示，上市公司跨界或者转型需要投资并购、投后管理、运营管理、战略提升四项能力，目前威创在沿着这四个方面发力，但很多公司在投资并购能力这方面不过关。威创股份投资并购非常重视两点，首先，不仅仅着眼于一个并购，而是着力于并购背后的投融资体系的整体规划;其次，更重视通过战略梳理、激励制度设计等手段激发并购标的的发展潜力。在财务报表利润外，更重视现金流的管理，由此建立投融资的良性循环。</p>\r\n\r\n<p>近年来教育行业呈现了良好的发展态势，有望成为中国社会经济领域重要的增长点。幼儿教育行业普遍存在连锁幼儿园集中度低，优质教育内容较少，缺乏高效、系统和先进的管理理念和体系等问题，上市公司垂青教育资产，需要提高优质教育资源整合和教育服务供给的能力。</p>\r\n\r\n<p><strong>所以跨界教育，必须做好长期经营的打算。</strong></p>\r\n\r\n<p>快速扩张的后遗症便是当资本涌入后，如果没有看到所期望的收益，一旦资本撤出，可能会造成行业的大片伤亡，</p>\r\n\r\n<p>此外，上市公司在并购教育标的、进入教育领域前，应做好前期调研并时时关注新主业发展的动态，学习教育行业运营管理经验以及人才的引进。</p>\r\n\r\n<p>有证券分析师对财联社表示，收购其实只是第一步，最难的是收购标的如何与主业相协同匹配。收购也让很多企业陷入泥潭、放弃主业，因为人都是贪婪的，都想去做轻松的生意。所以，在收购的同时，一定还要做透主业。</p>\r\n";
+    public static final String text = "<section data-color=\"rgb(245, 245, 244)\" data-custom=\"rgb(245, 245, 244)\" data-id=\"708\" data-tools=\"135编辑器\"> \n <section> \n  <section>\n    在未来，每个成功的区块链应用都是一家生态型“未来公司”。 \n   <br> &nbsp; \n   <br> 目前，包括“比特币”、“ 以太币”在内的区块链应用，市值都超过了1000亿美元。 \n   <br> \n   <br> \n   <br> 但是，主流经济学家及投资人都认为这些数字资产毫无价值，纯粹是“泡沫”、“投机”。 \n   <br> &nbsp; \n   <br> \n   <br> 断言区块链技术能创造“未来公司”，似乎为时尚早。 \n   <br> \n   <br> \n   <br> 2016年，第一个完全区块链化的公司项目就是“The DAO”，其融资额已高达1.6亿美元，很受追捧，但是因黑客攻击而饱受争议。 \n   <br> &nbsp; \n   <br> 不过，400年前股份公司、证券交易所诞生之初，也是如此受到质疑。 \n   <br> \n   <br> \n   <br> 那么，区块链技术和Token经济模型，如何促进公司新的演变？ \n   <br> \n   <br> \n   <br> 也许，答案就在华为虚拟股权制度里。 \n  </section> \n </section> \n</section> &nbsp; \n<br> \n<br> \n<strong>区块链技术：能否谱写未来公司法</strong> \n<br> \n<br> \n<br> 区块链，本质上是一种分布式的共识与价值激励的技术，一方面通过数学和算法实现了整个系统的共识与信任（规则与交易），另一方面通过代币（token）保证了生态体系的价值激励（财富记录）。 \n<br> \n<br> \n<br> 他有三个特点： \n<strong>分布式去中心化、无须第三方信任体系、数据不可以篡改。</strong> \n<br> \n<br> \n<br> \n<br> 我们可以看看比特币区块链、 以太坊区块链是什么。 \n<br> &nbsp; \n<br> \n<strong>比</strong>特币区块链，目前最大和最成功的区块链应用，其白皮书开明宗义指出其本质是“一种点对点的电子现金系统” 。 \n<br> &nbsp; \n<br> 在这个区块链系统中，根据一套协议（共识），“比特币”可以不依赖特定的中心机构发行，它是节点（矿工）通过工作量证明机制（POW，Proof of Work）获得参与到系统的权利（创建区块、验证交易），根据“挖矿”而产生，并把它记录在去中心化的“账本系统”中。 \n<br> \n<br> \n<br> \n<strong>&nbsp;“比特币”只是比特币区块链上的一条记录，一种可信的数字凭证。</strong> \n<strong>比特币区块链是“去中心化的数字货币公司”，创造“数字货币”是<strong>其核心价值</strong>。</strong> \n<br> &nbsp; \n<br> \n<strong>以</strong>太坊区块链，其定义是下一代智能合约和去中心化应用平台。简单地说， \n<strong>以太坊 = “数字货币” + 智能合约</strong>。 \n<br> \n<br> \n<br> 以太坊能让用户创建并管理一个去中心化的应用程序商店，是“区块链行业的苹果应用商店”公司。 \n<br> &nbsp; \n<br> 以太坊区块链系统的价值激励，发行 “ 以太币”，也是通过POW机制发行。 \n<br> \n<br> \n<br> 可见，其在比特币基础上进行了升级，其不仅是“去中心化的 数字货币公司”功能，同时还构建了一个创建区块链应用的功能，同时，其“ 数字货币”发行构成与比特币更复杂，其设置了一部分Pre-mine（矿前），解决了 以太坊区块链本身运营的资金问题。 \n<br> &nbsp; \n<br> 2014年8月， 以太坊众筹大约发行了7200万 以太币（“矿前”），之后，每年通过POW机制产量在7200万 以太币的25%（每年 以太币的矿产量，不高于1800万，有限制）。 \n<br> \n<br> \n<br> \n<strong>“The DAO”项目，就是基于以太坊的应用。</strong> \n<br> \n<br> \n<div style=\"text-align: center;\"> \n <img alt=\"未来的公司：区块链技术+Token经济\" data-=\"\" data-copyright=\"0\" data-fail=\"0\" data-ratio=\"0.88984375\" data-s=\"300,640\" data-type=\"png\" data-w=\"1280\" src=\"https://esongtest.oss-cn-shanghai.aliyuncs.com/news/20180129/lm1517209233500.jpg\"> \n</div> &nbsp; \n<br> 以上两款区块链应用，会发现其本质上与公司具有某种相似性： \n<br> &nbsp; \n<br> \n<strong>无论比特币区块链还是以太坊区块链，根本上是一个网络协议（规则）的产物，网络协议规定了价值（比特币、以太币）生成规则、交易规则、数据交换格式等。</strong> \n<br> \n<br> \n<br> 网络协议只规定使用这个协议的用户的行为。系统中任何节点只要在行为上满足这个协议的要求，就可以被网络接受，而这些行为可以由任何代码实现，并不需要某个特定的代码。 \n<br> &nbsp; \n<br> \n<strong>而关于公司的性质，新古典经济学认为其“乃一系列合约的连结”。</strong> \n<br> &nbsp; \n<br> 这“一系列合约关系”，包括与原材料或服务的卖方签订的供应合同，与向企业提供劳动力的个人签订的雇佣合同，与债券持有人、银行及其他资本供应方签订的借贷合同以及与企业产品的购买方签订的销售合同等。 \n<br> &nbsp; \n<br> \n<strong>区块链是一个开放式的标准合同，类似《公司法》，而比特币区块链、以太坊区块链等应用，类似一家基于区块链的公司。</strong> \n<br> \n<br> \n<br> 公司的起源，是人类协作的需要，特别是大规模的协作，股份公司则是人类的一项成就，它改写了人与人协作的秩序，国与国竞争的规则。 \n<br> &nbsp; \n<br> 股份公司发展今天，一方面在自由市场中创造了巨额财富，另一方面由于其建立在资本为中心和资本市场基础上，资本天然追求集中（垄断），造成了世界财富分配的极端不均。 \n<br> \n<br> \n<br> \n<section> \n <section> \n  <section> \n   <section> \n    <strong>驯服：财富创造与分配的怪兽？</strong> \n   </section> \n  </section> \n </section> \n</section> \n<br> \n<br> 人类所有的财富创造经济活动，从原始发展到现代，归功于自由市场经济机制，过去40年中国经济成就也是证明。 \n<br> &nbsp; \n<br> 自由市场体系或许是人类已知的最好的财富创造制度，但有着相当的脆弱性质。一方面是来自国家层面中心化共识的脆弱，另外一方面则是来自市场主体本身的中心化（垄断）趋势。 \n<br> &nbsp; \n<br> \n<strong>公司，天然追求资本集中和控制。</strong> \n<br> \n<br> \n<br> 2017年《财富》世界500强排行榜企业总营业收入为27.6万亿美元，占全球GDP总量37.3%（74万亿美元），超过了1/3。 \n<br> &nbsp; \n<br> 在互联网时代，这一特征尤为明显，美国有五巨头“ FAMGA”中国有“BAT”，未来全球市值最大的10家公司将均为互联网科技公司。 \n<br> \n<br> 结果显而易见，以美国为例，根据美国国会预算办公室报告显示： \n<br> \n<br> \n<br> 2013年，全美前10%最富有就家庭拥有占有了全美家庭财富总和的76%，第51%-第90%阶层的家庭占有23%，而全美最底层的民众仅占有全美家庭财富的1%。 \n<br> &nbsp; \n<br> \n<strong>公司导致的集中化，本质原因是资本的一元化，即现行公司是以货币资本为中心的，作为人的劳动并不能作为资本成为公司资产。</strong> \n<br> &nbsp; \n<br> 现阶段无法靠消灭“市场”与“公司”解决这一问题。市场配置资源，实现财富创造；而公司是在企业家主导下，作为产权载体与降低市场自由交易成本的最好的工具。 \n<br> \n<br> \n<br> \n<strong>区块链技术的出现，将改变当前以资本为中心的“股份公司”现状，进化成为货币资本、人力资本以及其他要素资本融合的组织</strong>。 \n<br> \n<br> &nbsp; \n<br> 这对现行法律制度和监管体系是巨大挑战，不过 \n<strong>先行者已有成功的实验</strong>。 \n<br> \n<br> \n<br> \n<section label=\"Copyright Reserved by ipaiban.com.\"> \n <section> \n  <strong><strong><strong>华为虚拟股权：成功运行的 Token ？</strong></strong></strong> \n </section> \n</section> \n<br> 我们可以看看华为的成长历程。 \n<br> \n<br> \n<br> 30年前，华为从2万注册资本起步，成长为中国科技企业的名片，预计2017年的收入将超过6000亿元人民币。 \n<br> &nbsp; \n<br> 1987年，任正非与5位合伙人共同投资成立深圳市华为技术有限公司(即华为公司前身)，注册资本仅2万元。 \n<br> &nbsp; \n<br> 3年后，华为公司即自称实行广泛的“员工持股制度”。 \n<br> &nbsp; \n<br> 2001年7月，华为股东大会通过了股票期权计划，推出了《华为技术有限公司虚拟股票期权计划暂行管理办法》。 \n<br> &nbsp; \n<br> 推出虚拟受限股之后，华为公司员工所持有的原股票被逐步消化吸收转化成虚拟股，原本就不具实质意义的实体股明确变为虚拟股。 \n<br> &nbsp; \n<br> 华为2016年报显示， \n<strong>员工持股计划参与人数为8.2万人</strong>，其中任正非占公司总股本的比例约1.4%。 \n<br> &nbsp; \n<br> 华为虚拟股权，和区块链项目有些类似： \n<br> \n<strong>1.&nbsp;&nbsp; 新产权框架：共享制，非资本主导</strong> \n<br> \n<strong>2.&nbsp;&nbsp; 虚拟股权制：类似Token的虚拟股权，而非《公司法》下的股权</strong> \n<br> \n<strong>3.&nbsp;&nbsp; 配股制度：类似发行机制，实现人力资本的转化</strong> \n<br> &nbsp; \n<br> 将华为虚拟股权作为一个区块链项目来看，可类比之处主要是以上三个关键点，但并非完全意义上的区块链项目。 \n<br> &nbsp; \n<br> \n<strong>主要是华为虚拟股权没有去中心化的记账，也是饱受争议之处：</strong> \n<br> &nbsp; \n<br> \n<br> 《金融时报》在一篇报道中这样描述华为虚拟股权登记—— \n<br> \n<br> \n<br> \n<em>“在华为深圳总部的一间密室里，有一个玻璃橱柜，里面放了10本蓝色的册子，这些厚达数公分的册子里记录着约80000名员工的姓名、身份证号码以及其他个人信息……向外国记者首次展示持股簿册，是华为所做努力的一部分，目的是反驳有关华为在股权问题上一直不够透明的批评。”</em> \n<br> &nbsp; \n<br> 《财经》杂志中对华为虚拟股权登记这样描述—— \n<br> \n<br> \n<br> \n<em>“每年此时，表现优异的华为技术有限公司（下称华为公司）员工们会被主管叫到办公室里去，这是他们一年当中最期待的时刻。这些华为公司的“奋斗者”们会得到一份合同，告知他们今年能够认购多少数量公司股票。这份合同不能被带出办公室，签字完成之后，必须交回公司保管，没有副本，也不会有持股凭证，但员工通过一个内部账号，可以查询自己的持股数量。”</em> \n<br> &nbsp; \n<br> \n<strong>但是，这完全不影响华为虚拟股权制度，视为伟大的区块链项目实验。</strong> \n<br> \n<br> \n<br> \n<section label=\"Copyright Reserved by ipaiban.com.\"> \n <section> \n  <strong>公司2.0：区块链 技术+Token 经济时代</strong> \n </section> \n</section> \n<br> \n<br> 公司进化的大幕已经开启。 \n<br> \n<br> \n<br> \n<strong>华为的实验，说明通过发行类似Token的权利证明，将人力资本作为财富创造与财富分配的要素，是一个有效的机制。</strong> \n<br> &nbsp; \n<br> 经济学家科斯认为，公司（企业）存在的意义，在于降低市场交易成本。 \n<br> &nbsp; \n<br> \n<strong>区块链技术和Token经济模型，可以对公司进行升级，解决市场交易成本问题，在数字世界建立一套，以去中心化的方式实现财富证明、财富流动、资源配置与分工协作的价值激励系统。</strong> \n<br> \n<br> \n<br> 现在，有了 以太坊及其订立的 ERC20标准，任何人都可以在 以太坊上发行自定义的 token，这个token 可以代表任何权益和价值。 \n<br> &nbsp; \n<br> \n<strong>这种基于区块链技术发行Token的新物种，就是公司的2.0版本</strong>。 \n<br> &nbsp; \n<br> 不过，我国尚未建立起“ 虚拟货币”及ICO（Initial CoinOffering）的监管体系，中国人民银行等七部委联合发布了《关于防范代币发行融资风险的公告》，禁止了ICO、禁止交易平台从事代币、“ 虚拟货币”兑换买卖业务。 \n<br> \n<br> \n<br> 本文重新审视华为虚拟股权制度，以区块链技术和Token经济模型，将人力资本引入现行公司框架，形成股权+Token的二元结构，建立一种更好的公司系统，实现整个社会财富创造的风险共担，利益共享。 \n<br> \n<br> \n<br> \n<strong>或再诞生十个或百个华为。<br> <br> 来源微信公众号：法务VC</strong> \n<br>";
 
     @BindView(R.id.webView)
-    WebView mWebView;
+    DrawWebView mWebView;
     @BindView(R.id.titleBar)
     TitleBar mTitleBar;
     @BindView(R.id.subtitle)
@@ -249,8 +250,10 @@ public class NewsDetailActivity extends BaseActivity {
     private void initScrollViewLocation() {
         mTitleHeight = mTitleLayout.getMeasuredHeight();
         //webView加载完高度才完全展示出来，这时候scrollView才能滑动到记忆的位置
-        if (mNewsDetail != null)
+        if (mNewsDetail != null){
             mScrollView.scrollTo(0, mNewsDetail.getReadHeight());
+            Log.e("zzz","starty:"+mNewsDetail.getReadHeight()+" and webview 高度:"+mWebView.getHeight());
+        }
     }
 
     private void initScrollView() {
@@ -278,33 +281,6 @@ public class NewsDetailActivity extends BaseActivity {
         }else{
             updateData(mNewsDetail);
         }
-//        if (mNewsDetail == null) {
-//            mNewsDetail = new NewsDetail();
-//            mNewsDetail.setContent("<p>彭博称，北欧最大的银行已经告诉其员工，不要交易比特币和其他<u>加密货币</u>。这可能导致欧洲银行联合会评估其对<u>加密货币</u>的官方立场。</p> \n<p>北欧联合银行发言人Afroditi Kellberg通过电话说，该行将于2月28日实施这一禁令，之前该行董事会同意采取这一立场，因为<u>加密货币</u>市场“无监管的性质”。截至第三季度末，该行约有3.15万名员工。</p> \n<p>北欧联合银行在发给彭博的电子邮件中表示，其政策“包括对现有持币人员的过渡性规定，并允许某些例外”。已经拥有比特币的员工“被允许保留现有持仓”。</p> \n<div>\n  北欧第二大银行丹麦丹斯克银行说，不鼓励员工交易比特币，但尚未决定是否需要全面禁止。丹斯克银行发言人Kenni Leth在一封电子邮件中表示：“我们对 \n <u>加密货币</u>持怀疑态度，并建议我们的员工不要交易，但我们并没有实施实际的禁令。我们正在分析情况，时间会告诉我们是否会有正式的禁令。” \n <p></p> \n <p>丹斯克银行不向客户提供<u>加密货币</u>交易。“由于各种<u>加密货币</u>不成熟、缺乏透明度，我们决定在我们的平台上不提供这种证券交易，”Leth说。</p> \n <p>北欧联合银行在电邮中表示，对客户的政策与对员工不一样，但也要强调不推荐客户投资于此。</p> \n <p>（原文标题：《北欧联合银行全员禁止比特币 因市场“无监管”》）</p> \n</div> \n<div></div> \n<div>\n  来源：新浪财经 \n</div> \n<div>\n  文章地址：http://finance.sina.com.cn/stock/usstock/c/2018-01-23/doc-ifyqwiqi5439089.shtml \n</div>");
-//            mNewsDetail.setCreateTime(1516781844505L);
-//            mNewsDetail.setDetail("比特币资讯");
-//            mNewsDetail.setId("5a6841148940b041d86256da");
-//            mNewsDetail.setImgs(new ArrayList<String>());
-//            mNewsDetail.setPraiseCount(0);
-//            mNewsDetail.setReaderCount(1);
-//            mNewsDetail.setReleaseTime(1516636800000L);
-//            mNewsDetail.setSecondDetail("行业解读");
-//            mNewsDetail.setSecondType(12);
-//            mNewsDetail.setSource("BTCif中文网");
-//            mNewsDetail.setSummary("彭博称，北欧最大的银行已经告诉其员工，不要交易比特币和其他加密货币。这可能导致欧洲银行联合会评估其对加密货币的官方立场。");
-//            List<String> tags = new ArrayList<>();
-//            tags.add("银行");
-//            tags.add("员工");
-//            tags.add("北欧");
-//            tags.add("加密");
-//            tags.add("货币");
-//            mNewsDetail.setTags(tags);
-//            mNewsDetail.setTitle("北欧联合银行全员禁止比特币，因加密货币市场“无监管”");
-//            mNewsDetail.setType(1);
-//        }
-//        updateData(mNewsDetail);
-
-
     }
 
     private void updateData(NewsDetail newsDetail) {
@@ -353,6 +329,7 @@ public class NewsDetailActivity extends BaseActivity {
     public void onBackPressed() {
         super.onBackPressed();
         mNewsDetail.setReadHeight(mScrollView.getScrollY());
+        Log.e("zzz","y:"+mScrollView.getScrollY());
         NewsCache.insertOrReplaceNews(mNewsDetail);
     }
 
