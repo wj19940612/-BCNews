319c3a584d808d9dee1593811d4641e6cd53c3cc
houcancan <houcancan@hashxy.com>
2018-02-02 10:21:52 +0800

    修改资讯详情分享

--- app/src/main/java/com/sbai/bcnews/App.java
+++ app/src/main/java/com/sbai/bcnews/App.java
@@ -82,7 +82,7 @@ public class App extends Application {
 
     static {
         // 注意：测试用 appId & secret
-        PlatformConfig.setWeixin("wx967daebe835fbeac", "5bb696d9ccd75a38c8a0bfe0675559b3");
+        PlatformConfig.setWeixin("wx7576ec9bb65aea1a", "d640a05c70ec272f56557bd7e9c15dc4");
         PlatformConfig.setQQZone("1106465763", "KEYqYrMZDFn2dn5KQhP");
     }
 }
