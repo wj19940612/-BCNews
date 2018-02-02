86917f249eaa4c5cd355758859d5f784dea8efa8
王杰 <wangjie@hashxy.com>
2018-02-02 10:02:01 +0800

    增加启动页

---
+++ app/src/main/java/com/sbai/bcnews/activity/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.sbai.bcnews.activity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.sbai.bcnews.R;
+import com.sbai.bcnews.utils.Launcher;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class SplashActivity extends BaseActivity {
+
+    private static final int SPLASH_PAGE_STAY_TIME = 1500;
+
+    @BindView(R.id.bgSplash)
+    ImageView mBgSplash;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+
+        mBgSplash.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                goMain();
+            }
+        }, SPLASH_PAGE_STAY_TIME);
+
+    }
+
+    private void goMain() {
+        Launcher.with(getActivity(), MainActivity.class).execute();
+        supportFinishAfterTransition();
+    }
+}
