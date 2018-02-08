05bb7009d45c818c4a6ac0f1ed4289773d8e7152
王杰 <wangjie@hashxy.com>
2018-02-08 15:08:10 +0800

    完善我的页面

--- app/src/main/java/com/sbai/bcnews/fragment/MineFragment.java
+++ app/src/main/java/com/sbai/bcnews/fragment/MineFragment.java
@@ -2,6 +2,8 @@ package com.sbai.bcnews.fragment;
 
 
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.text.SpannableString;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +12,11 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.sbai.bcnews.R;
+import com.sbai.bcnews.activity.mine.LoginActivity;
 import com.sbai.bcnews.model.LocalUser;
+import com.sbai.bcnews.model.UserInfo;
+import com.sbai.bcnews.utils.Launcher;
+import com.sbai.bcnews.utils.StrUtil;
 import com.sbai.bcnews.view.IconTextRow;
 import com.sbai.glide.GlideApp;
 
@@ -58,7 +64,29 @@ public class MineFragment extends BaseFragment {
 
     private void updateUserLoginStatus() {
         updateUserHeadPortrait();
+        UserInfo userInfo = LocalUser.getUser().getUserInfo();
+        if (LocalUser.getUser().isLogin()) {
+            mUserName.setText(userInfo.getUserName());
+            updateUserCollectNumber(userInfo.getCollectCount());
+            updateUserReadHistory(userInfo.getReadCount());
+        } else {
+            mUserName.setText(R.string.click_login);
+            updateUserCollectNumber(0);
+        }
+    }
 
+    private void updateUserReadHistory(int readHistory) {
+        SpannableString spannableString = StrUtil.mergeTextWithColor(getString(R.string.history),
+                " " + readHistory,
+                ContextCompat.getColor(getActivity(), R.color.cyan));
+        mHistory.setText(spannableString);
+    }
+
+    private void updateUserCollectNumber(int collectNumber) {
+        SpannableString spannableString = StrUtil.mergeTextWithColor(getString(R.string.collect),
+                " " + collectNumber,
+                ContextCompat.getColor(getActivity(), R.color.cyan));
+        mHistory.setText(spannableString);
     }
 
     private void updateUserHeadPortrait() {
@@ -68,7 +96,7 @@ public class MineFragment extends BaseFragment {
                     .placeholder(R.drawable.ic_default_head_portrait)
                     .circleCrop()
                     .into(mHeadPortrait);
-        }else {
+        } else {
             GlideApp.with(getActivity())
                     .load(R.drawable.ic_default_head_portrait)
                     .circleCrop()
@@ -86,10 +114,19 @@ public class MineFragment extends BaseFragment {
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.headPortrait:
-                break;
             case R.id.userName:
+                if (LocalUser.getUser().isLogin()) {
+                    // TODO: 2018/2/8 用户资料页
+                } else {
+                    Launcher.with(getActivity(), LoginActivity.class).execute();
+                }
                 break;
             case R.id.collect:
+                if (LocalUser.getUser().isLogin()) {
+                    // TODO: 2018/2/8  收藏页面
+                } else {
+                    Launcher.with(getActivity(), LoginActivity.class).execute();
+                }
                 break;
             case R.id.history:
                 break;
--- app/src/main/java/com/sbai/bcnews/model/UserInfo.java
+++ app/src/main/java/com/sbai/bcnews/model/UserInfo.java
@@ -10,6 +10,36 @@ public class UserInfo implements Parcelable {
 
     private String userPortrait;
 
+    private String userName;
+
+    private int collectCount;
+
+    private int readCount;
+
+    public int getCollectCount() {
+        return collectCount;
+    }
+
+    public void setCollectCount(int collectCount) {
+        this.collectCount = collectCount;
+    }
+
+    public int getReadCount() {
+        return readCount;
+    }
+
+    public void setReadCount(int readCount) {
+        this.readCount = readCount;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
     public String getUserPortrait() {
         return userPortrait;
     }
@@ -18,27 +48,29 @@ public class UserInfo implements Parcelable {
         this.userPortrait = userPortrait;
     }
 
-    public String getUserPhone() {
-        //TODO 需要字段
-        return null;
-    }
-
-    protected UserInfo(Parcel in) {
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-    }
-
     @Override
     public int describeContents() {
         return 0;
     }
 
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.userPortrait);
+        dest.writeString(this.userName);
+    }
+
+    public UserInfo() {
+    }
+
+    protected UserInfo(Parcel in) {
+        this.userPortrait = in.readString();
+        this.userName = in.readString();
+    }
+
     public static final Creator<UserInfo> CREATOR = new Creator<UserInfo>() {
         @Override
-        public UserInfo createFromParcel(Parcel in) {
-            return new UserInfo(in);
+        public UserInfo createFromParcel(Parcel source) {
+            return new UserInfo(source);
         }
 
         @Override
