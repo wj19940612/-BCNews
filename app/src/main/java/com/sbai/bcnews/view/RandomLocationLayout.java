c239e8a3fe8216856071228c84fd6ea900644b58
wangjie <wangjie@hashxy.com>
2018-06-19 15:08:43 +0800

    调整挖矿界面动画和ui

--- app/src/main/java/com/sbai/bcnews/view/RandomLocationLayout.java
+++ app/src/main/java/com/sbai/bcnews/view/RandomLocationLayout.java
@@ -1,10 +1,18 @@
 package com.sbai.bcnews.view;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -30,6 +38,11 @@ public class RandomLocationLayout extends LinearLayout {
 
     private static final String TAG = "RandomLocationLayout";
 
+    private static final int MINING_VIEW_INDEX = 1;
+    private static final int EMPTY_VIEW_INDEX = 0;
+
+    private static final int MINTING_VIEW_DURATION = 2000;
+
     private final int maxBrightCount = 10;
 
     private List<QKC> mQksList;
@@ -44,6 +57,7 @@ public class RandomLocationLayout extends LinearLayout {
     private int receiveCount;
     private int mDefaultRandomMargin = 10;
     private int mViewWidth;
+    private TextView mEmptyTextView;
 
     public RandomLocationLayout(Context context) {
         this(context, null);
@@ -66,13 +80,15 @@ public class RandomLocationLayout extends LinearLayout {
     }
 
     private void resetView() {
-        ViewGroup parent = (ViewGroup) getChildAt(0);
+        int childCount = getChildCount();
+        Log.d(TAG, "resetView: " + childCount);
+        ViewGroup parent = (ViewGroup) getChildAt(MINING_VIEW_INDEX);
         for (int i = 0; i < parent.getChildCount(); i++) {
             TextView qkcCoin = (TextView) parent.getChildAt(i);
             qkcCoin.setText(null);
             qkcCoin.setEnabled(true);
             qkcCoin.setVisibility(INVISIBLE);
-            setRandomTranslate(qkcCoin, i);
+//            setRandomTranslate(qkcCoin, i);
         }
     }
 
@@ -80,6 +96,32 @@ public class RandomLocationLayout extends LinearLayout {
         randomIndex = new ArrayList<>();
         mViewWidth = (int) (Display.getScreenWidth() - Display.dp2Px(50, getResources()) * 2);
         mDefaultRandomMargin = (int) (mViewWidth * 0.013);
+        createEmptyView();
+    }
+
+    private void createEmptyView() {
+        if (mEmptyTextView == null) {
+            mEmptyTextView = new TextView(getContext());
+            mEmptyTextView.setText(R.string.mining);
+            mEmptyTextView.setTextColor(Color.WHITE);
+            mEmptyTextView.setGravity(Gravity.CENTER);
+            mEmptyTextView.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_mining, 0, 0);
+            mEmptyTextView.setCompoundDrawablePadding((int) Display.dp2Px(4, getResources()));
+            LayoutParams layoutParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+            layoutParams.gravity = Gravity.CENTER;
+
+            addView(mEmptyTextView, layoutParams);
+        }
+
+        startEmptyViewAnimator();
+    }
+
+    private void startEmptyViewAnimator() {
+        if (mEmptyTextView == null) return;
+        ObjectAnimator translationY = ObjectAnimator.ofFloat(mEmptyTextView, "translationY", 100, -100);
+        translationY.setDuration(MINTING_VIEW_DURATION).setRepeatMode(ValueAnimator.REVERSE);
+        translationY.setRepeatCount(ObjectAnimator.INFINITE);
+        translationY.start();
     }
 
     /**
@@ -95,7 +137,7 @@ public class RandomLocationLayout extends LinearLayout {
         if (stopSize > mQksList.size()) {
             stopSize = mQksList.size();
         }
-        ViewGroup parent = (ViewGroup) getChildAt(0);
+        ViewGroup parent = (ViewGroup) getChildAt(MINING_VIEW_INDEX);
         while (index < stopSize) {
             Integer e = mRandom.nextInt(parent.getChildCount());
             if (!randomIndex.contains(e)) {
@@ -103,24 +145,43 @@ public class RandomLocationLayout extends LinearLayout {
                 index++;
             }
         }
-
+        if (mQksList.isEmpty()) {
+            showEmptyView();
+        }
         setCoinStyle(stopSize);
     }
 
     public void setQksList(List<QKC> qksList) {
-        mQksList = qksList;
-        randomDataIndex();
+        if (qksList != null && !qksList.isEmpty()) {
+            showMiningView();
+            mQksList = qksList;
+            randomDataIndex();
+        } else {
+            showEmptyView();
+        }
+    }
+
+    private void showMiningView() {
+        mEmptyTextView.setVisibility(GONE);
+        mEmptyTextView.clearAnimation();
+        getChildAt(MINING_VIEW_INDEX).setVisibility(VISIBLE);
+    }
+
+    private void showEmptyView() {
+        getChildAt(MINING_VIEW_INDEX).setVisibility(GONE);
+        mEmptyTextView.setVisibility(VISIBLE);
+        startEmptyViewAnimator();
     }
 
     private void setCoinStyle(int stopSize) {
-        removeAllViews();
+        removeView(getChildAt(MINING_VIEW_INDEX));
         View view = LayoutInflater.from(getContext()).inflate(R.layout.layout_random_location_qkc, this, false);
-        addView(view);
+        addView(view, MINING_VIEW_INDEX);
         resetView();
-        ViewGroup parent = (ViewGroup) getChildAt(0);
+        ViewGroup parent = (ViewGroup) getChildAt(MINING_VIEW_INDEX);
         final int listSize = randomIndex.size();
         for (int i = 0; i < listSize; i++) {
-            TextView qkcCoin = (TextView) parent.getChildAt(randomIndex.get(i));
+            final TextView qkcCoin = (TextView) parent.getChildAt(randomIndex.get(i));
             final QKC qks = mQksList.get(i);
             qkcCoin.setVisibility(VISIBLE);
             qkcCoin.setText(getContext().getString(R.string.plus_qks, FinanceUtil.formatWithScaleRemoveTailZero(qks.getIntegral())));
@@ -131,9 +192,16 @@ public class RandomLocationLayout extends LinearLayout {
                     if (mOnCoinClickListener != null) {
                         mOnCoinClickListener.onCoinClick(v, qks);
                     }
-
                 }
             });
+            int time = 1200 + i * 30;
+//            if (i == 2) {
+            ObjectAnimator translationY = ObjectAnimator.ofFloat(qkcCoin, "translationY", -10, 10);
+            translationY.setDuration(time).setRepeatMode(ValueAnimator.REVERSE);
+            translationY.setRepeatCount(ObjectAnimator.INFINITE);
+            translationY.start();
+
+//            }
         }
     }
 
@@ -143,14 +211,32 @@ public class RandomLocationLayout extends LinearLayout {
      * @param v   被点击的按钮
      * @param qks QKS数据
      */
-    public void removeCoin(View v, QKC qks) {
-        mQksList.remove(qks);
-        ((TextView) v).setText(null);
-        v.setVisibility(INVISIBLE);
-        receiveCount++;
-        if (receiveCount >= randomIndex.size() || receiveCount == maxBrightCount) {
-            randomDataIndex();
+    public void removeCoin(final View v, final QKC qks) {
+
+        final AnimationDrawable animationDrawable = (AnimationDrawable) ContextCompat.getDrawable(getContext(), R.drawable.animation_mining_cancel);
+        ((TextView) v).setCompoundDrawablesWithIntrinsicBounds(null, animationDrawable, null, null);
+        ((TextView) v).setText(" ");
+        animationDrawable.start();
+
+        int duration = 0;
+        for (int i = 0; i < animationDrawable.getNumberOfFrames(); i++) {
+            duration += animationDrawable.getDuration(i);
         }
+
+        v.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                animationDrawable.stop();
+                mQksList.remove(qks);
+                ((TextView) v).setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_qkc, 0, 0);
+                (v).setVisibility(INVISIBLE);
+                receiveCount++;
+                if (receiveCount >= randomIndex.size() || receiveCount == maxBrightCount) {
+                    randomDataIndex();
+                }
+            }
+        }, duration);
+
     }
 
     private void setRandomTranslate(View view, int i) {
@@ -233,4 +319,23 @@ public class RandomLocationLayout extends LinearLayout {
     public void setOnCoinClickListener(OnCoinClickListener onCoinClickListener) {
         mOnCoinClickListener = onCoinClickListener;
     }
+
+    private Animation.AnimationListener animationListener = new Animation.AnimationListener() {
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
+    };
+
+
 }
