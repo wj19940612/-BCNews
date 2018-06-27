2a35c232083124415a97ef4d6ec528d52f85abdf
Zhang <375215380@qq.com>
2018-06-27 15:38:13 +0800

    fix bug

--- app/src/main/java/com/sbai/bcnews/fragment/MineFragment.java
+++ app/src/main/java/com/sbai/bcnews/fragment/MineFragment.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.text.SpannableString;
@@ -111,6 +112,11 @@ public class MineFragment extends BaseFragment {
         }
     };
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        registerLoginBroadcast();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
